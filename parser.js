d3dc85061564dfd471a08d423d5c65aa4d35deec
Rafhael Josino Lima <rafhael_josino1@hotmail.com>
2021-07-27 19:58:40 -0300

    parser update

--- parser.js
+++ parser.js
@@ -41,18 +41,28 @@ const ExcelJS = require('exceljs');
 
 let workbook = new ExcelJS.Workbook();
 
+// Object that represents a cell of the datasheet
 function Navigator(column, line) {
     this.column = column;
     this.line = line;
     this.pos = () => {return this.column + this.line}
-    this.parse = () => this.line++
+    this.parseColumn = () => {
+
+    }
+    this.parseLine = () => this.line++
 }
 
 /*
 First formate data, separe each crypto, even with data redundance
 */
 
-function CryptoCurrency() {
+function CryptoOp(date, mediumPrice, remainQuant) {
+    this.date = date;
+    this.mediumPrice = mediumPrice;
+    this.remainQuant = remainQuant;
+}
+
+function CryptoReport() {
     this.lastAqDate = 0;
     this.lastAqValue = 0;
     this.lastLOValue = 0;
@@ -65,9 +75,12 @@ const matchCrypto = new RegExp('');
 const matchOp = new RegExp('');
 const operationsList = [];
 
-// ############################### Operation Functions #############################
+// ############################### Operation Parsers #############################
+/*
+The functions are called by the parser, which passes its position in the datasheet to be used as reference
+*/
 // Operation type 1 - Buying using Real (R$)
-function opTypeOne() {
+function opTypeOne(reference) {
 
 }
 
@@ -79,7 +92,7 @@ workbook.xlsx.readFile('Criptos.xlsx').then(() => {
         const cell = worksheet.getCell(parser.pos()).value;
         if (cell) {
             console.log(parser.pos(), ":", cell);
-            parser.parse();
+            parser.parseLine();
             parsing();
         }
         else {
