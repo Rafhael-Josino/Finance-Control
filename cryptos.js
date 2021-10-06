c0b137bb5c8847a14aa744b3f64e580f162da1ea
Rafhael Josino Lima <rafhael_josino1@hotmail.com>
2021-10-06 15:56:09 -0300

    sellings displayed in another column for each crypto (not to all ops yet)

--- cryptos.js
+++ cryptos.js
@@ -1,6 +1,7 @@
 let menu = document.getElementById("menu");
-let opList = document.getElementById("opList");
-let purchases, sells;
+let purchasesList = document.getElementById("purchasesList");
+let sellingsList = document.getElementById("sellingsList");
+let purchases, sellings;
 let buttonBTC = document.getElementById("BTC");
 let buttonETH = document.getElementById("ETH");
 let buttonLTC = document.getElementById("LTC");
@@ -8,9 +9,21 @@ let buttonLTC = document.getElementById("LTC");
 fetch('operations').then(resp => resp.json()).
 	then(jsonFile => {
 		purchases = jsonFile.purchases;
+		sellings = jsonFile.sellings;
+		console.log(typeof purchases);
+		console.log(typeof sellings);
 		//opList.innerHTML = JSON.stringify(jsonFile, undefined, 2);
 	})
 
-buttonBTC.addEventListener("click", () => opList.textContent = JSON.stringify(purchases[0], undefined, 2));
-buttonETH.addEventListener("click", () => opList.textContent = JSON.stringify(purchases[1], undefined, 2));
-buttonLTC.addEventListener("click", () => opList.textContent = JSON.stringify(purchases[2], undefined, 2));
+buttonBTC.addEventListener("click", () => {
+	purchasesList.textContent = JSON.stringify(purchases[0], undefined, 2);
+	sellingsList.textContent = JSON.stringify(sellings[0], undefined, 2);
+});
+buttonETH.addEventListener("click", () => {
+	purchasesList.textContent = JSON.stringify(purchases[1], undefined, 2);
+	sellingsList.textContent = JSON.stringify(sellings[1], undefined, 2);
+});
+buttonLTC.addEventListener("click", () => {
+	purchasesList.textContent = JSON.stringify(purchases[2], undefined, 2);
+	sellingsList.textContent = JSON.stringify(sellings[2], undefined, 2);
+});
