19ccccabae44db16504b6b63c9dea3cd2481481e
Rafhael Josino Lima <rafhael_josino1@hotmail.com>
2021-12-12 16:47:53 -0300

    Preparing to insert a menu that shows the current situation of each crypto in the wallet

--- cryptos.js
+++ cryptos.js
@@ -2,6 +2,8 @@ let menu = document.getElementById("menu");
 let purchasesList = document.getElementById("purchasesList");
 let sellingsList = document.getElementById("sellingsList");
 let purchases, sellings;
+let select = document.getElementById("select");
+let currentSitDiv = document.getElementById("currentSituationDiv");
 let buttonBTC = document.getElementById("BTC");
 let buttonETH = document.getElementById("ETH");
 let buttonLTC = document.getElementById("LTC");
@@ -18,6 +20,31 @@ fetch('operations').then(resp => resp.json()).
 		console.log(typeof purchases);
 		console.log(typeof sellings);
 	})
+
+// Compute the current situation of each crypto:
+// total, medium price, aquisition price
+// total = sum of remanescent quantities of each purchase operation
+// medium price = final medium price attribute of each purchase element
+// This parameters will be used to do the checksum of the purchases operations with the user's manual control
+
+let test = "this is just a test";
+
+select.addEventListener("change", (event) => {
+	if (event.target.value === "operations") {
+		currentSitDiv.innerHTML = null;
+	}
+	if (event.target.value === "currentSituation") {
+		currentSitDiv.innerHTML = test;
+	purchasesList.textContent = null;
+	sellingsList.textContent = null;
+	}
+});
+
+// Will be used to substitute the buttons for a select element
+function selectCrypto(event) {
+	purchasesList.textContent = JSON.stringify(purchases[event.target.value], undefined, 2);
+	sellingsList.textContent = JSON.stringify(sellings[event.target.value], undefined, 2);
+}
 
 buttonBTC.addEventListener("click", () => {
 	purchasesList.textContent = JSON.stringify(purchases[0], undefined, 2);
@@ -64,3 +91,4 @@ buttonPAX.addEventListener("click", () => {
 	purchasesList.textContent = JSON.stringify(purchases[7], undefined, 2);
 	sellingsList.textContent = JSON.stringify(sellings[7], undefined, 2);
 })
+
