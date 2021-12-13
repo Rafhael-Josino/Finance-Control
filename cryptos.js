c76f4de6f828d0b70a96901b63cafa1b72d10a3b
Rafhael Josino Lima <rafhael_josino1@hotmail.com>
2021-12-12 22:19:17 -0300

    Buttons changed by select element

--- cryptos.js
+++ cryptos.js
@@ -2,15 +2,9 @@ let menu = document.getElementById("menu");
 let purchasesList = document.getElementById("purchasesList");
 let sellingsList = document.getElementById("sellingsList");
 let purchases, sellings;
-let select = document.getElementById("select");
+let selectMenu = document.getElementById("selectMenu");
+let selectCrypto = document.getElementById("selectCrypto");
 let currentSitDiv = document.getElementById("currentSituationDiv");
-let buttonBTC = document.getElementById("BTC");
-let buttonETH = document.getElementById("ETH");
-let buttonLTC = document.getElementById("LTC");
-let buttonEOS = document.getElementById("EOS");
-let buttonUSDT = document.getElementById("USDT");
-let buttonTUSD = document.getElementById("TUSD");
-let buttonPAX = document.getElementById("PAX");
 let cryptoImage = document.getElementById("image");
 
 fetch('operations').then(resp => resp.json()).
@@ -29,7 +23,7 @@ fetch('operations').then(resp => resp.json()).
 
 let test = "this is just a test";
 
-select.addEventListener("change", (event) => {
+selectMenu.addEventListener("change", (event) => {
 	if (event.target.value === "operations") {
 		currentSitDiv.innerHTML = null;
 	}
@@ -41,54 +35,11 @@ select.addEventListener("change", (event) => {
 });
 
 // Will be used to substitute the buttons for a select element
-function selectCrypto(event) {
+function selectCryptoFunc(event) {
 	purchasesList.textContent = JSON.stringify(purchases[event.target.value], undefined, 2);
 	sellingsList.textContent = JSON.stringify(sellings[event.target.value], undefined, 2);
+	cryptoImage.src="images/" + stringify(event.target.value);
+	cryptoImage.alt="Crypto";
 }
 
-buttonBTC.addEventListener("click", () => {
-	purchasesList.textContent = JSON.stringify(purchases[0], undefined, 2);
-	sellingsList.textContent = JSON.stringify(sellings[0], undefined, 2);
-	cryptoImage.src="images/bitcoin-icon-22.jpg";
-	cryptoImage.alt="bitcoin";
-});
-buttonETH.addEventListener("click", () => {
-	purchasesList.textContent = JSON.stringify(purchases[1], undefined, 2);
-	sellingsList.textContent = JSON.stringify(sellings[1], undefined, 2);
-	cryptoImage.src="images/ethereum-icon-28.jpg";
-	cryptoImage.alt="ether";
-});
-buttonLTC.addEventListener("click", () => {
-	purchasesList.textContent = JSON.stringify(purchases[2], undefined, 2);
-	sellingsList.textContent = JSON.stringify(sellings[2], undefined, 2);
-	cryptoImage.src="images/litecoin-2210.png";
-	cryptoImage.alt="litecoin";
-});
-
-buttonEOS.addEventListener("click", () => {
-	purchasesList.textContent = JSON.stringify(purchases[3], undefined, 2);
-	sellingsList.textContent = JSON.stringify(sellings[3], undefined, 2);
-	cryptoImage.src="images/eos-coin-2213.png";
-})
-
-buttonUSDT.addEventListener("click", () => {
-	purchasesList.textContent = JSON.stringify(purchases[4], undefined, 2);
-	sellingsList.textContent = JSON.stringify(sellings[4], undefined, 2);
-	cryptoImage.src="images/tether-coin-2212.png";
-})
-
-buttonTUSD.addEventListener("click", () => {
-	purchasesList.textContent = JSON.stringify(purchases[5], undefined, 2);
-	sellingsList.textContent = JSON.stringify(sellings[5], undefined, 2);
-})
-
-buttonUSDC.addEventListener("click", () => {
-	purchasesList.textContent = JSON.stringify(purchases[6], undefined, 2);
-	sellingsList.textContent = JSON.stringify(sellings[6], undefined, 2);
-})
-
-buttonPAX.addEventListener("click", () => {
-	purchasesList.textContent = JSON.stringify(purchases[7], undefined, 2);
-	sellingsList.textContent = JSON.stringify(sellings[7], undefined, 2);
-})
-
+selectCrypto.addEventListener("change", selectCryptoFunc);
