e944f1d2d386aaac5f187da3f3942cc30650799e
Rafhael Josino Lima <rafhael_josino1@hotmail.com>
2021-12-16 23:35:03 -0300

    select element removed, logs accessed directly in the table at the page's begin

--- cryptos.js
+++ cryptos.js
@@ -2,8 +2,8 @@ let menu = document.getElementById("menu");
 let purchasesList = document.getElementById("purchasesList");
 let sellingsList = document.getElementById("sellingsList");
 let purchases, sellings;
-let selectMenu = document.getElementById("selectMenu");
 let selectCrypto = document.getElementById("selectCrypto");
+let currentSitTab = document.getElementById("currentSituationTab");
 let currentSitDiv = document.getElementById("currentSituationDiv");
 let cryptoImage = document.getElementById("image");
 let summaries = [];
@@ -15,22 +15,22 @@ function CryptoSituation(name, total, mediumPrice) {
 	this.aquisitionPrice = total * mediumPrice;
 }
 
-function selectCryptoFunc(event) {
-	purchasesList.textContent = JSON.stringify(purchases[event.target.value], undefined, 2);
-	sellingsList.textContent = JSON.stringify(sellings[event.target.value], undefined, 2);
-	cryptoImage.src="images/" + String(event.target.value);
-	cryptoImage.alt="Crypto";
+function cryptoLogConstruct(cryptoIndex, purchases, sells) {
+	function cryptoLog() {
+		purchasesList.textContent = JSON.stringify(purchases[cryptoIndex], undefined, 2);
+		sellingsList.textContent = JSON.stringify(sells[cryptoIndex], undefined, 2);
+	}
+	return cryptoLog;
 }
 
 fetch('operations').then(resp => resp.json()).
 	then(jsonFile => {
 		purchases = jsonFile.purchases;
 		sellings = jsonFile.sellings;
-		console.log(typeof purchases);
-		console.log(typeof sellings);
 		let cryptosList = purchases.filter(p => p.length); // Filter out empty lists
+		let sellingsTemp = sellings.filter(p => p.length);
 		console.log(cryptosList);
-		const cryptoQuantity = cryptosList.length; // WRONG
+		const cryptoQuantity = cryptosList.length;
 		console.log(cryptoQuantity);
 		for (let i = 0; i < cryptoQuantity; i++) {
 			let total = 0;
@@ -38,21 +38,18 @@ fetch('operations').then(resp => resp.json()).
 				total = total + cryptosList[i][j].remainQuant;
 			}
 			console.log(cryptosList[i]);
+			let newLine = document.createElement("tr");
+			newLine.insertCell(-1);
+			newLine.lastChild.innerHTML = cryptosList[i][0].asset;
+			newLine.insertCell(-1);
+			newLine.lastChild.innerHTML = total;
+			newLine.insertCell(-1);
+			newLine.lastChild.innerHTML = cryptosList[i][cryptosList[i].length-1].newMediumPrice;
+			newLine.insertCell(-1);
+			newLine.lastChild.innerHTML = total * cryptosList[i][cryptosList[i].length-1].newMediumPrice;
+			newLine.setAttribute("class", "newLineClass");
+			newLine.addEventListener("click", cryptoLogConstruct(i, cryptosList, sellingsTemp));
+			currentSitTab.appendChild(newLine);
 			summaries.push(new CryptoSituation(cryptosList[i][0].asset, total, cryptosList[i][cryptosList[i].length-1].newMediumPrice));
 		}
 	})
-
-selectMenu.addEventListener("change", (event) => {
-	if (event.target.value === "operations") {
-		currentSitDiv.innerHTML = null;
-		selectCrypto.addEventListener("change", selectCryptoFunc);
-	}
-	if (event.target.value === "currentSituation") {
-		currentSitDiv.innerHTML = JSON.stringify(summaries, undefined, 2);
-		purchasesList.textContent = null;
-		sellingsList.textContent = null;
-		selectCrypto.removeEventListener("change", selectCryptoFunc);
-	}
-});
-
-selectCrypto.addEventListener("change", selectCryptoFunc);
