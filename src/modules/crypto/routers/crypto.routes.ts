0f8793cf37b8c233f5d7ba60e347baf5f8899d49
Rafhael Josino Lima <rafhael_josino1@hotmail.com>
2022-04-19 22:13:10 -0300

    middleware functions written in crypto.routes.ts erased

--- src/modules/crypto/routers/crypto.routes.ts
+++ src/modules/crypto/routers/crypto.routes.ts
@@ -2,6 +2,7 @@ import { Router, Request, Response } from 'express';
 import path from 'path';
 import fs from 'fs';
 
+
 // ###################### Middleware #########################
 import { CryptoUserVerifications } from '../middlewares/CryptoUserVerificationsPG';
 //import { CryptoUserVerifications } from '../middlewares/CryptoUserVerifications';
@@ -18,77 +19,11 @@ import { deleteUserController } from '../useCases/deleteUser';
 import { getUserController } from '../useCases/getUser';
 import { getSheetController} from '../useCases/getSheet';
 
+
 const cryptoRoutes = Router();
 
-// ----------------------- Middlewares -----------------------------------------
-// This declarations will be totally substituted by an imported class containing them
-// Also, the userName variable will be obtained from the request's header
-
-
-function verifyUserExists(req: Request, res: Response, next: any): any {
-	const { userName } = req.params;
-
-	fs.readdir(path.join(__dirname, '..', '..', '..', '..', 'logs', 'cryptos'), (err, files) => {
-		if (err) {
-			console.log("Server here - unable to read directory:", err);
-			res.status(500).json({error: "Server here - unable to read directory: " + err.message});
-		}
-		else {
-			if (files.includes(`${userName}.json`)) {
-				return next();
-			}
-			else {
-				console.log(`Server message: ${userName} does not exist`);
-				return res.status(404).json({error: "User does not exist"});
-			}
-		}
-	})
-}
-
-function verifyXLSXexists(req: Request, res: Response, next: any): any {
-	const { userName } = req.params;
-
-	fs.readdir(path.join(__dirname, '..', '..', '..', '..', 'logs', 'cryptos'), (err, files) => {
-		if (err) {
-			console.log("Server here - unable to read directory:", err);
-			res.status(500).json({error: "Server here - unable to read directory: " + err.message});
-		}
-		else {
-			console.log(files);
-			if (files.includes(`${userName}.xlsx`)) {
-				return next();
-			}
-			else {
-				console.log(`Server message: ${userName} 's XLSX file does not exist`);
-				return res.status(404).json({error: "User's XLSX file does not exist"});
-			}
-		}
-	})	
-}
-
-function verifyUserAlreadyExists(req: Request, res: Response, next: any): any {
-	const { userName } = req.body;
-
-	fs.readdir(path.join(__dirname, '..', '..', '..', '..', 'logs', 'cryptos'), (err, files) => {
-		if (err) {
-			console.log("Server here - unable to read directory:", err);
-			res.status(500).json({error: "Server here - unable to read directory: " + err.message});
-		}
-		else {
-			console.log(files);
-			if (files.includes(`${userName}.json`)) {
-				console.log("Server message: User already exists");
-				return res.status(500).json({error: "User already exists"});
-			}
-			else {
-				return next();
-			}
-		}
-	})
-}
 
 // ----------------------- Routes -----------------------------------------
-
 
 cryptoRoutes.get("/cryptos", (req, res) => {
 	const namePath = path.join(__dirname, '..', 'pages', 'cryptos.html');
