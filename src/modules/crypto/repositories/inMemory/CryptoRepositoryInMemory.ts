1c91d351a74a08747dd91b32c3beb41553800325
rafhael josino <rafhael_josino1@hotmail.com>
2022-08-19 14:46:44 -0300

    inMemory crypto repository (for tests) realocated

--- src/modules/crypto/repositories/inMemory/CryptoRepositoryInMemory.ts
+++ src/modules/crypto/repositories/inMemory/CryptoRepositoryInMemory.ts
@@ -1,5 +1,5 @@
 import { ICryptoRepository, IGetSheetOperationsDTO, IGetSheetResponse, IPostSheetOperationsDTO, IReferenceSheet } from "@modules/crypto/repositories/ICryptoRepository";
-import { CryptoSheet, CryptoSummary } from "../models/Cryptos";
+import { CryptoSheet, CryptoSummary } from "../../infra/postgresSQL/models/Cryptos";
 
 class CryptoRepositoryInMemory implements ICryptoRepository {
     cryptoSheets: CryptoSheet[];
