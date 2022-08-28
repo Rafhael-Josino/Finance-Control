1702a345bda38dcd9ed17bf453b7df542fc66a47
rafhael josino <rafhael_josino1@hotmail.com>
2022-08-27 21:57:06 -0300

    user token table added to DB

---
+++ src/shared/infra/postgresSQL/migrations/createUserTokensTable.ts
@@ -0,0 +1,31 @@
+import { PG } from '..';
+
+class Migration {
+  async up() {
+    await PG.query(
+      `CREATE TABLE user_tokens (
+        user_token_id char(21) default nanoid(),
+        refresh_token varchar,
+        created_at timestamp default current_timestamp,
+        expires_date timestamp,
+        user_id char(21),
+        constraint pk_tokens
+          primary key (user_token_id),
+        constraint fk_users
+          foreign key (user_id)
+            references users (user_id)
+            on delete cascade
+      );`,
+      []
+    );
+  }
+
+  async down() {
+    await PG.query(
+      `DROP TABLE user_tokens CASCADE;`,
+      []
+    );
+  }
+}
+
+export { Migration }
