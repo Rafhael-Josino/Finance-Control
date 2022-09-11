3ecd00fcc60286e6fda4011ea5edade9ca3790ea
rafhael josino <rafhael_josino1@hotmail.com>
2022-09-11 08:44:33 -0300

    creates refresh token and saves it in DB

--- src/modules/accounts/useCases/authenticateAccount/sessionUseCase.ts
+++ src/modules/accounts/useCases/authenticateAccount/sessionUseCase.ts
@@ -1,10 +1,13 @@
-import { IAccountRepository } from '../../repositories/IAccountRepository';
 import { inject, injectable } from 'tsyringe';
 import { compare } from 'bcrypt';
 import { sign } from 'jsonwebtoken';
-import fs from 'fs';
-import path from 'path';
+
+import { IAccountRepository } from '../../repositories/IAccountRepository';
+import { IUserTokenRepository } from '@modules/accounts/repositories/IUserTokenRepository';
 import { AppError } from '@shared/errors/AppErrors';
+import auth from '@config/auth';
+import { IDateProvider } from '@shared/container/providers/dateProvider/IDateProvider';
+
 
 interface IRequest {
     userName: string;
@@ -14,36 +17,53 @@ interface IRequest {
 interface IResponse {
     userName: string;
     token: string;
+    refresh_token: string;
 }
 
 @injectable()
 class SessionUseCase {
     constructor(
         @inject("AccountRepository")
-        private accountRepository: IAccountRepository
+        private accountRepository: IAccountRepository,
+        @inject("UserTokenRepository")
+        private userTokenRepository: IUserTokenRepository,
+        @inject("DateProvider")
+        private dateProvider: IDateProvider
     ) {}
 
     async execute( { userName, password }: IRequest ): Promise<IResponse> {
         const usersList = await this.accountRepository.listUsers();
-        if (!usersList.includes(userName)) 
+        if (!usersList.includes(userName)) {
+            console.log("Username or password incorrect")
             throw new AppError("Username or password incorrect", 403);
+        }
         
         const user = await this.accountRepository.getUser(userName);
+        
         const passwordMatch = await compare(password, user.password);
-        if (!passwordMatch) 
+        if (!passwordMatch) {
+            console.log("Username or password incorrect")
             throw new AppError("Username or password incorrect", 403);
+        }
 
-        // MD5 phrase
-        const token = sign({}, "ad9da275a544684b597372149318f020", {
+        const token = sign({}, auth.secret_token, {
             subject: user.id,
-            expiresIn: "1d"
+            expiresIn: auth.expires_in_token,
+        });
+        
+        const refresh_token = sign({}, auth.secret_refresh_token, {
+            subject: user.id,
+            expiresIn: auth.expires_in_refresh_token,
         });
 
-        const session = { userName, token };
+        await this.userTokenRepository.createUserToken({
+            user_id: user.id,
+            refresh_token,
+            created_at: this.dateProvider.addDays(0),
+            expires_date: this.dateProvider.addDays(auth.expires_in_refresh_token_Days),
+        })
 
-        const tokenJsonFile = path.join(
-            __dirname, '..', '..', '..', '..', 'utils', 'tokens', `${userName}.json`);
-        fs.writeFileSync(tokenJsonFile, JSON.stringify(session));        
+        const session = { userName, token, refresh_token };
 
         return session;
     }
